a0736673b86dd94e41a8d09fadaa5514ef8a72c2
3abglil <mks.abogalil666@gmail.com>
2023-05-17 15:09:21 +0300

    fully working login form
    waiting for the database checking to be a valid login form , now it logs in if user name and password are admin to the admin dashboard and the to the home page if user name and password are not empty

--- server.js
+++ server.js
@@ -1,99 +1,99 @@
-const express = require('express')
-const path=require('path')
-const signup_model2 = require('./models/signupschema')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const signup_model2 = require("./models/signupschema");
 
 //Read the current directory name
 
 const hostname = "127.0.0.1";
 const port = 8080;
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.static('public/js'));
-app.use(express.static('public/resources'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true})); // for post method 
-mongoose.connect("mongodb+srv://seif2102995:travi231@travinature.3akvybv.mongodb.net/")
-  .then( (result) => {
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public/js"));
+app.use(express.static("public/resources"));
+app.set("view engine", "ejs");
+// app.use(express.urlencoded({extended:true})); // for post method
+app.use(bodyParser.urlencoded({ extended: true })); // for post method
+mongoose
+  .connect(
+    "mongodb+srv://seif2102995:travi231@travinature.3akvybv.mongodb.net/"
+  )
+  .then((result) => {
     app.listen(3010);
-    console.log('connected to db successfully');
+    console.log("connected to db successfully");
   })
-  .catch( (err) => {
+  .catch((err) => {
     console.log(err);
-  }); 
-
-
-
-app.get('/', function(req, res) {
-    res.render('home');
   });
 
-  app.get('/book', function(req, res) {
-    res.render('map');
-  });
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-  app.get('/admin', function(req, res) {
-    res.render('admin');
-  });
-  app.get('/signup', function(req, res) {
-    res.render('signup');
-  });
-  app.get('/argentina', function(req, res) {
-    res.render('argentina');
-  });
-  app.get('/eg', function(req, res) {
-    res.render('eg');
-  });
-  app.get('/about', function(req, res) {
-    res.render('about');
-  });
-  app.get('/login', function(req, res) {
-    res.render('login');
-  });
-  app.get('/reports-admin',(req,res)=>{
-    res.render('reports-admin');
-  })
+app.get("/book", function (req, res) {
+  res.render("map");
+});
 
-  app.post('/signup', (req,res)=>{
-    // const fst = document.getElementsByClassName("firstNext");
-    // const sec = document.getElementsByClassName("next-1");
-    // const trd = document.getElementsByClassName("next-2");
-    // if(fst.clicked==true){
-    //   if(sec.clicked==true){
-    //     if(trd.clicked==true){
-    //       if(document.getElementsByClassName('submit').clicked==true){
-      const signup_data = new signup_model2(req.body);
-      // console.log("555555");
-              // console.log('clicked');
-              // console.log('firstname ' +   req.body.firstname)
-            const obj = req.body;
-            let count =0;
-            for (const key in req.body) {
-                if(obj[key]!==""){
-                  count++;
-                }
-              // console.log(`${key}: ${req.body[key]}   + mn for each`);
-          }
-          console.log('count = ' + count )
-          if(count==8){
-              signup_data.save().then(()=>{console.log('saved to db')}).catch(()=>{console.log('error')});
-          }
-              console.log(req.body);  
-            // }else{
-            //   console.log("bbbbbb");
-            // }
-    //     }
-    //   }
-    // }
-    
-  });
+app.get("/admin", function (req, res) {
+  res.render("admin");
+});
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+app.get("/argentina", function (req, res) {
+  res.render("argentina");
+});
+app.get("/eg", function (req, res) {
+  res.render("eg");
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.post("/login", (req, res) => {
+  if (req.body.username == "admin") {
+    if (req.body.password == "admin") {
+      res.render("admin");
+    }
+  } else if (req.body.username !== "") {
+    if (req.body.password !== "") {
+      res.render("home");
+    }
+  } else {
+    // errors
+  }
+});
 
+app.get("/reports-admin", (req, res) => {
+  res.render("reports-admin");
+});
 
+app.post("/signup", (req, res) => {
+  const signup_data = new signup_model2(req.body);
+  const obj = req.body;
+  let count = 0;
+  for (const key in req.body) {
+    if (obj[key] !== "") {
+      count++;
+    }
+  }
+  console.log("count = " + count);
+  if (count == 8) {
+    signup_data
+      .save()
+      .then(() => {
+        console.log("saved to db");
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+  console.log(req.body);
+});
 
-// connecting to database
-
-  
-
-  app.listen(8080);
-  console.log("listening on port " +port);
+app.listen(8080);
+console.log("listening on port " + port);
