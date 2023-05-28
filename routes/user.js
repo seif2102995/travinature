64948ffeae2a8c1289907346d08efe1c7d94f5c1
3abglil <mks.abogalil666@gmail.com>
2023-05-29 01:41:54 +0300

    header adjusted to all login types

--- routes/user.js
+++ routes/user.js
@@ -11,21 +11,24 @@ router.get("/login", (req, res)=> {
   req.session.user=req.body.username;
   res.render("login", { user: (req.session.user === undefined ? "" : req.session.user) });
 });
-
+router.post("/login", login,async (req, res)=> {
+  const check = await signup_model.findOne({username:req.body.username});
+  req.session.user=req.body.username;
+  res.render('home',{ user: (req.session.user === undefined ? "" : check) })
+  });
 router.get("/signup", function (req, res) {
   req.session.user=req.body.username;
   res.render("signup",{ user: (req.session.user === undefined ? "" : req.session.user) });
 });
 
-router.post('/signup' ,handleSignup,(req,res)=>{
+router.post('/signup' ,handleSignup, async (req,res)=>{
+  const check = await signup_model.findOne({username:req.body.username});
   req.session.user=req.body.username;
-  console.log(req.session.user)
-  res.render('home',{ user: (req.session.user === undefined ? "" : req.session.user) })
-  console.log('in routerrrrrrrrrrrrrrrr + ');
+  res.render('home',{ user: (req.session.user === undefined ? "" : check) })
 });
 
 
-router.post('/checkUN', checkUN);
+// router.post('/checkUN', checkUN);
 
 
 
@@ -40,9 +43,7 @@ router.use((req, res, next) => {
       res.render('error', { err: 'You must login to access this page', user: (req.session.user === undefined ? "" : req.session.user) })
   }
 });
-router.post("/login", login, (req, res)=> {
 
-});
 
 router.get('/logout', (req, res) => {
   req.session.destroy();
