55f96c093a5aeb9c2585b8977d0e268725dd3c89
wagiihh <ibrahim2105690@miuegypt.edu.eg>
2023-05-31 02:47:32 +0300

    tripsSchema

---
+++ models/tripsSchema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+
+const tripsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Tripss = mongoose.model("Tripss", tripsSchema);
+
+export  {Tripss};
