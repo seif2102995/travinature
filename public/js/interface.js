86ef0afe2444f3792b650d29eaac32a0757e221a
moazzmuhammed <mks.abogalil666@gmail.com>
2023-06-05 06:16:13 +0300

    dynamic country paging almost done

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,14 +1,35 @@
 
-let ids = [];
-(function () {
-  let elements = document.getElementsByTagName("path");
-  for (let i = 0; i < elements.length; i++) {
-    let element = elements[i];
-    if (element.id) {
-        ids.push(element.getAttribute("name"));
-    }
-  }
-  ids.sort();
-})();
+// let ids = [];
+// (function () {
+//   let elements = document.getElementsByTagName("path");
+//   for (let i = 0; i < elements.length; i++) {
+//     let element = elements[i];
+//     if (element.id) {
+//         ids.push(element.getAttribute("name"));
+//     }
+//   }
+//   ids.sort();
+// })();
 
-sessionStorage.setItem('options',JSON.stringify(ids));
+// sessionStorage.setItem('options',JSON.stringify(ids));
+
+
+const gello = "angola";
+fetch('/book/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ gello })
+})
+  .then(response => response.json())
+  .then(data => console.log(data + "HIII"))
+  .catch(error => console.error(error + " HOOO"));
+
+
+
+// const elementId = "argentina";
+// fetch(`/?elementId=${elementId}`)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
--- routes/booking.js
+++ routes/booking.js
@@ -3,17 +3,23 @@ import fs from "fs";
 
 const router = Router();
 
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   res.render("map",{ user: (req.session.user === undefined ? "" : req.session.user) });
 });
 
-// router.get("/country", function (req, res, next) {
-//   fs.readFile('../travinature/routes/ao.svg', 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data); // Log the SVG file contents to the console
-//     res.render("country",{data});
-//   });
-// });
+
+router.get("/:class", async function (req, res, next) {
+    fs.readFile('../travinature/routes/ao.svg', 'utf8', (err, data) => {
+        if (err) throw err;
+        // console.log(data); // Log the SVG file contents to the console
+        res.render("country",{data});
+      });
+  // res.render("home",{ user: (req.session.user === undefined ? "" : req.session.user) });
+});
+
+
+
+
 
 router.get("/argentina", function (req, res, next) {
     res.render("argentina",{ user: (req.session.user === undefined ? "" : req.session.user) });
