d096ffe0c95b1b52b4b5ff262c4e405e55bab21d
3abglil <mks.abogalil666@gmail.com>
2023-06-06 23:33:00 +0300

    dynamic country paging done

--- routes/booking.js
+++ routes/booking.js
@@ -9,10 +9,11 @@ router.get("/",isAuth, async function (req, res, next) {
 });
 
 
-router.get("/:class",isAuth, async function (req, res, next) {
+router.get("/:class", async function (req, res, next) {
   let x = req.params.class;
+  console.log(x);
     fs.readFile(`../travinature/public/countries/${x}.svg`, 'utf8', (err, data) => {
-        if (err) throw err;
+        if (err) res.render("map",{ user: (req.session.user === undefined ? "" : req.session.user) });
         // console.log(data); // Log the SVG file contents to the console
         res.render("country",{data});
       });
@@ -23,9 +24,10 @@ router.get("/:class",isAuth, async function (req, res, next) {
 
 
 
-router.get("/argentina", function (req, res, next) {
-    res.render("argentina",{ user: (req.session.user === undefined ? "" : req.session.user) });
-  });
+
+// router.get("/argentina", function (req, res, next) {
+//     res.render("argentina",{ user: (req.session.user === undefined ? "" : req.session.user) });
+//   });
 
 
 export default router;
